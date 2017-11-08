6dce610d6c3896e21e699600baf7bae6faa18e26
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2017-11-08 18:40:55 +0100

    ComFactory: Adding special paths for win32com import

--- ComFactory/__init__.py
+++ ComFactory/__init__.py
@@ -14,6 +14,15 @@ if os.path.isdir(third_party_dir):
     Logger.log("d", "Found 3rd-party directory and adding in into PYTHONPATH.")
     sys.path.append(third_party_dir)
 
+# win32* specific things
+win32_dirs = [os.path.join(third_party_dir, "win32"),
+              os.path.join(third_party_dir, "win32", "lib"),
+              os.path.join(third_party_dir, "pywin32_system32"),
+              ]
+for win32_dir in win32_dirs:
+    if os.path.isdir(win32_dir):
+        sys.path.append(win32_dir)
+
 # Trying to import one of the COM modules
 try:
     from .PyWin32Connector import ComConnector
